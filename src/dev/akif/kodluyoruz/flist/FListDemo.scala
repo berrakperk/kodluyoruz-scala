68e90a0dfec4395aa4e4cfb03a323fa12bb20dd5
berrakperk <56256577+berrakperk@users.noreply.github.com>
2021-02-01 03:09:43 +0300

    Update FListDemo

--- src/dev/akif/kodluyoruz/flist/FListDemo.scala
+++ src/dev/akif/kodluyoruz/flist/FListDemo.scala
@@ -3,11 +3,25 @@ package dev.akif.kodluyoruz.flist
 object FListDemo {
   def main(args: Array[String]): Unit = {
     val list = (1 to 100 by 2).toList
-
     val flist1 = FList(1, 2, 3)
-
     val flist2 = FList(list)
-
     val flist3 = FList("hello", "world")
+    
+    //Append test
+    println(flist1.append(4).append(5))
+    
+    //Prepend test 
+    println(flist1.prepend(0).prepend(10))
+
+    //Concat test
+    println(flist2.concat(flist1))
+
+    //Filter test
+    println(flist1.filter(_ > 2))
+   
+    //Size test
+    println(flist3.size)
+    
+    
   }
 }
